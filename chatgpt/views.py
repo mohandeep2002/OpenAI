e1b56a01a5aebe94912e825b7023333b7bc11671
Syed Abdul Raheem <abdulraheem.s@holosuit.com>
2023-02-05 15:56:37 +0530

    search bar and submit button added

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -2,26 +2,30 @@ from django.shortcuts import render
 import requests
 
 def generate_response(request):
-    url = "https://api.openai.com/v1/engines/text-davinci-002/completions"
+  
+    if request.method == 'POST':  
+      url = "https://api.openai.com/v1/engines/text-davinci-002/completions"
 
-    prompt = "how to make omelette"
-    payload = {
-      "prompt": prompt,
-      "max_tokens": 128,
-      "n": 1,
-      "stop": None,
-      "temperature": 0.5,
-    }
-    headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer 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"
-    }
-    response = requests.post(url, json=payload, headers=headers).json()
-    # print(response) just for pushing
-    
-    generated_response = response["choices"][0]["text"]
+      prompt = request.POST['input']
+      payload = {
+        "prompt": prompt,
+        "max_tokens": 128,
+        "n": 1,
+        "stop": None,
+        "temperature": 0.5,
+      }
+      headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 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"
+      }
+      response = requests.post(url, json=payload, headers=headers).json()
+      # print(response) just for pushing
+      
+      generated_response = response["choices"][0]["text"]
 
-    return render(request, 'generated_response.html', {
-        'generated_response': generated_response,
-        'res': response
-        })
+      return render(request, 'generated_response.html', {
+          'generated_response': generated_response,
+          'res': response
+          })
+    else :
+      return render(request, 'generated_response.html')
